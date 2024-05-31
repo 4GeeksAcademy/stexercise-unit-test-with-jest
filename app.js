b09e4295eee5520cc1ed962d1bbf7b3f7ffc4640
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-05-31 18:40:50 +0000

    actualizado ST

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ let oneEuroIs = {
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
     // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
+    let theDollar = valueInEuro * 1.07;
     // Retornamos el valor en dólares
-    return valueInDollar;
+    return theDollar;
 }
 const fromDollarToYen = function(valueInDollar) {
-    let valueInYen = valueInDollar * 157.20;
-    return valueInYen;
+    let theYen = (valueInDollar / 1.07) * 156.5;
+    return theYen;
 }
 const fromYenToPound = function(valueInYen) {
-    let valueInPound = valueInYen * 0.005;
-    return valueInPound;
+    let thePound = (valueInYen / 156.5) * 0.87;
+    return thePound;
 }
 
 
--- test.js
+++ test.js
@@ -2,9 +2,9 @@ test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
     const fromEuroToDollar = function(valueInEuro) {
         // Convertimos el valor a dólares
-        let valueInDollar = valueInEuro * 1.07;
+        let theDollar = valueInEuro * 1.07;
         // Retornamos el valor en dólares
-        return valueInDollar;
+        return theDollar;
     }
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); 
@@ -12,18 +12,18 @@ test("One euro should be 1.07 dollars", function() {
 test("One dollar should be 157,20 yen", function() {
     // Importo la funcion desde app.js
     const fromDollarToYen = function(valueInDollar) {
-        let valueInYen = valueInDollar * 157.20;
-        return valueInYen;
+        let theYen = (valueInDollar / 1.07) * 156.5;
+        return theYen;
     }
     // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(4)).toBe(628.8); 
+    expect(fromDollarToYen(2)).toBe(292.5233644859813); 
 }); 
 test("One yen should be 0.005 pounds", function() {
     // Importo la funcion desde app.js
-        const fromYenToPound = function(valueInYen) {
-        let valueInPound = valueInYen * 0.005;
-        return valueInPound
+    const fromYenToPound = function(valueInYen) {
+        let thePound = (valueInYen / 156.5) * 0.87;
+        return thePound;
     }
     // Hago mi comparación (la prueba)
-    expect(fromYenToPound(100)).toBe(0.50); 
+    expect(fromYenToPound(100)).toBe(0.5559105431309904); 
 }); 
